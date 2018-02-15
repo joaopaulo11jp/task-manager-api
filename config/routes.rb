4791ea175a98372d01ad275d92ae5c0f5b61b2e3
João Paulo <joaopaulo11jp@gmail.com>
2018-02-15 23:46:28 +0000

    Definição inicial de namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #default: {format: :json} -> Define o MimeType de comunicação com o servidor
+  #constraints: { subdomain: 'api' } -> Define um subdomínio para o namespace
+  #path: "/" -> renomeia o path do namespace, o que ficaria host/api/whatever, ficará api.host/whatever
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: "/" do
+
+  end
 end
