5612e0931330600ac8b3205259912950e64a9c25
João Paulo <joaopaulo11jp@gmail.com>
2018-02-16 01:07:52 +0000

    Versionamento usando header HTTP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -5,8 +7,9 @@ Rails.application.routes.draw do
   #constraints: { subdomain: 'api' } -> Define um subdomínio para o namespace
   #path: "/" -> renomeia o path do namespace, o que ficaria host/api/whatever, ficará api.host/whatever
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: "/" do
-    namespace :v1 do
-      resources :tasks
+    #api.host:port/v1
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      
     end
   end
 end
