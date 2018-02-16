a203656f5b0a8f73638e00b0268e0ae400e7c28a
João Paulo <joaopaulo11jp@gmail.com>
2018-02-16 00:38:43 +0000

    Exemplo de versionamento de Api por Path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   #constraints: { subdomain: 'api' } -> Define um subdomínio para o namespace
   #path: "/" -> renomeia o path do namespace, o que ficaria host/api/whatever, ficará api.host/whatever
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: "/" do
-
+    namespace :v1 do
+      resources :tasks
+    end
   end
 end
