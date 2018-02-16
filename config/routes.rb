76cbeaefd139e20d0929645d3000bc902d9e0cdc
João Paulo <joaopaulo11jp@gmail.com>
2018-02-16 02:16:53 +0000

    Devise-> rails g devise:install DEPOIS rails g devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #default: {format: :json} -> Define o MimeType de comunicação com o servidor
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: "/" do
     #api.host:port/v1
     namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
-      
+
     end
   end
 end
