affb56990be140e2f1e7945985558e186d984872
joaopaulo11jp <joaopaulo11jp@gmail.com>
2018-02-20 21:35:00 -0300

    Testes iniciais com usuários devise sem shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @user = build(:user)
+  end
+
+  it do
+    expect(@user).to respond_to(:email)
+  end
+
+  #it { expect(@user).to respond_to(:name) } Vai falhar
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
+  it { expect(@user).to be_valid }
+  
 end
