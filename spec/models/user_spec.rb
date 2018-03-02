1472436d5b4c57e9c3d4562bce89756d958b8b92
João Paulo <joaopaulo11jp@gmail.com>
2018-03-02 01:51:18 +0000

    usando Subject vazio para testar model, ainda sem shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  before do 
-    @user = build(:user)
-  end
 
-  it do
-    expect(@user).to respond_to(:email)
-  end
+  #before { @user = build(:user) }
 
+  #it { expect(@user).to respond_to(:email) }
   #it { expect(@user).to respond_to(:name) } Vai falhar
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  #it { expect(@user).to respond_to(:password) }
+  #it { expect(@user).to respond_to(:password_confirmation) }
+  #it { expect(@user).to be_valid }
+
+  #Usando subject:
+
+  it { expect(subject).to respond_to(:email) }
+  #it { expect(subject).to respond_to(:name) } Vai falhar
+  it { expect(subject).to respond_to(:password) }
+  it { expect(subject).to respond_to(:password_confirmation) }
+  #it { expect(subject).to be_valid } Vai falhar, pois o subject equivale à User.new
+
   
 end
