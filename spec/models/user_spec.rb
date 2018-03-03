5894548cd0b3a691d1bbbd4544a21ef6e3b81bc4
João Paulo <joaopaulo11jp@gmail.com>
2018-03-03 01:48:35 +0000

    [Testes] Definição dos testes de usuário usando shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
+  #let(:user) { build(:user) }
   #O let funciona de forma LAZY, só será montado a partir do primeiro chamado
   #será criado um método 'user' que retornará as informações
   #Usa-se let! para que o objeto seja criado assim que bater na linha, não seria mais LAZY...
 
+  it{ is_expected.to validate_presence_of :email }
+  it{ is_expected.to validate_uniqueness_of(:email).case_insensitive } #email é case insesitive, portanto é necessário esepecificar no teste
+  it{ is_expected.to validate_confirmation_of :password }
+  it{ is_expected.to allow_value('joaopaulo11jp@gmail.com').for :email }
+  
+  ############################################################################################
+  
   #it { expect(user).to respond_to :email }
 
  # context 'when name is blank' do
