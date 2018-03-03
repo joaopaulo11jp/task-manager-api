dacafce496f5585cf737a2ff4b20bbb8767be4dc
João Paulo <joaopaulo11jp@gmail.com>
2018-03-03 01:36:49 +0000

    [Testes] Substituindo os contextos por teste de validade do shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
   #O let funciona de forma LAZY, só será montado a partir do primeiro chamado
-  # será criado um método 'user' que retornará as informações
+  #será criado um método 'user' que retornará as informações
   #Usa-se let! para que o objeto seja criado assim que bater na linha, não seria mais LAZY...
 
-  it { expect(user).to respond_to :email }
+  #it { expect(user).to respond_to :email }
 
-  context 'when name is blank' do
+ # context 'when name is blank' do
     #context agrupa os testes
-    before { user.name = " " }
+#    before { user.name = " " }
     #before será executado antes de cada teste do contexto
-    it { expect(user).not_to be_valid }
+#    it { expect(user).not_to be_valid }
 
-  end
+#  end
 
-  context 'when name is nil' do
-    before { user.name = nil }
+#  context 'when name is nil' do
+#    before { user.name = nil }
     
-    it { expect(user).not_to be_valid }
+#    it { expect(user).not_to be_valid }
     
-  end
+#  end
 
+  it { expect(user).to validate_presence_of(:name) }
   
 end
