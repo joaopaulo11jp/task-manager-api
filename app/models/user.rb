cba46d8ad43cd9756e5fb754147defa5e3e5cca5
João Paulo <joaopaulo11jp@gmail.com>
2018-03-03 01:13:20 +0000

    [Utilidade] Atributos não persistidos.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :name
+  # attr_accessor cria um atributo não persistido
+
+  validates_presence_of :name
+  # atributos não persistidos também podem ter validação
+
+
 end
