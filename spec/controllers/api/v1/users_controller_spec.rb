40daa823fdd931a36965b29de4770d83494de49d
João Paulo <joaopaulo11jp@gmail.com>
2018-03-06 00:31:24 +0000

    [Controller] Criando controller de Users... comentário

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
-
+#Para testes de controller de API, não é bacana usar 'type: :controller'
+#Pois seriam testes de método de controller, sem fazer requisições
 RSpec.describe Api::V1::UsersController, type: :controller do
 
 end
