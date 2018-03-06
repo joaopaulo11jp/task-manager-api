b21004cfd47d8046057e3153f409937b45a40b71
João Paulo <joaopaulo11jp@gmail.com>
2018-03-06 01:55:55 +0000

    [Testes] Comportamento para usuário inexistente

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -17,6 +17,17 @@ RSpec.describe 'Users API', type: :request do
               user_response = JSON.parse(response.body)
               expect(user_response["id"]).to eq(user_id)
             end
+
+            it "returns status code 200" do
+              expect(response).to have_http_status(200)
+            end
+
+            it "when the user doesnt exists" do
+              let(:user_id) { 100000 } #Sobrescreve o :user_id para apenas para esse teste
+              #o before é executado a cada it, neste, o mesmo considerará user_id = 100000
+              
+              expect(response).to have_http_status(404)
+            end
         end
     end
 end
